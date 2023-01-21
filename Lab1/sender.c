87954b532d4a7570e84de82a4902fafd4ab6d0a6
Gunin Wasan <guninw1001@gmail.com>
2023-01-21 15:24:34 -0500

    general edits

--- Lab1/sender.c
+++ Lab1/sender.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
 // main goal: to make use of UDP and send between client and server.
-// author: shikhar chaurasia
-// student number: 1006710016
+/*
+Done by: 
+Shikhar Chaurasia (Student # 1006710016) 
+and
+Gunin Wasan (Student # 1007147749)
+*/
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
+    printf("Hi");
+    return 0;
 }
--- Lab1/server.c
+++ Lab1/server.c
@@ -3,8 +3,13 @@
 #include <sys/socket.h>
 
 // main goal: to make use of UDP and send between client and server.
-// author: shikhar chaurasia
-// student number: 1006710016
+
+/*
+Done by: 
+Shikhar Chaurasia (Student # 1006710016) 
+and
+Gunin Wasan (Student # 1007147749)
+*/
 
 int main(int argc, char **argv)
 {
