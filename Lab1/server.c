77511999ee87ab35969c6ddc951ff460a7991c79
Shikhar Chaurasia <81959120+paxcentauri@users.noreply.github.com>
2023-01-21 23:16:20 -0500

    Update server.c

--- Lab1/server.c
+++ Lab1/server.c
@@ -1,21 +1,3 @@
-// #include <stdio.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-
-// // main goal: to make use of UDP and send between client and server.
-
-
-
-// int main(int argc, char **argv)
-// {
-//     if (argc != 2)
-//         exit(0);
-//     int port_number = argv[1];
-//     int srv_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-//     int bind_value = bind(srv_socket_fd, )
-// }
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
