ba4e2906aa69c202f958341a8aed4daf446120ed
Shikhar Chaurasia <shikhar.chaurasia@mail.utoronto.ca>
2023-02-05 12:03:24 -0500

    Configured the deliver directory

--- Lab1/deliver.c
+++ Lab1/deliver.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -17,30 +18,6 @@ Shikhar Chaurasia (Student # 1006710016)
 and
 Gunin Wasan (Student # 1007147749)
 */
-
-// int
-// timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
-// {
-//   /* Perform the carry for the later subtraction by updating y. */
-//   if (x->tv_usec < y->tv_usec) {
-//     int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-//     y->tv_usec -= 1000000 * nsec;
-//     y->tv_sec += nsec;
-//   }
-//   if (x->tv_usec - y->tv_usec > 1000000) {
-//     int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-//     y->tv_usec += 1000000 * nsec;
-//     y->tv_sec -= nsec;
-//   }
-
-//   /* Compute the time remaining to wait.
-//      tv_usec is certainly positive. */
-//   result->tv_sec = x->tv_sec - y->tv_sec;
-//   result->tv_usec = x->tv_usec - y->tv_usec;
-
-//   /* Return 1 if result is negative. */
-//   return x->tv_sec < y->tv_sec;
-// }
 // struct packet
 // {
 //     unsigned int total_frag;
@@ -141,6 +118,9 @@ int main(int argc, char **argv)
             if (does_file_exist(file_name) == true)
             {
                 // send protocol (ftp) to the server
+                struct timeval start;
+                gettimeofday(&start, NULL);
+
                 if (sendto(srv_socket_fd, protocol, strlen(protocol), 0, server_info->ai_addr, server_info->ai_addrlen) == -1)
                 {
                     perror("sendto\n");
@@ -161,6 +141,13 @@ int main(int argc, char **argv)
                     close(srv_socket_fd);
                     exit(0);
                 }
+                struct timeval end;
+                gettimeofday(&end, NULL);
+                struct timeval result;
+                printf("Time taken is : %ld micro seconds\n",
+                    ((end.tv_sec * 1000000 + end.tv_usec) -
+                    (start.tv_sec * 1000000 + start.tv_usec)));
+
                 char *message = "yes";
                 // if received message was yes
                 if (strcmp(text_buffer, message) == 0)
