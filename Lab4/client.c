3ed2d8f7a6d3fc858ae2ab7f95a80ac63e7c24a7
Shikhar Chaurasia <shikhar.chaurasia@mail.utoronto.ca>
2023-03-12 19:39:29 -0400

    Adding all the define constants into server and client.

--- Lab4/client.c
+++ Lab4/client.c
@@ -12,6 +12,30 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define LOGIN 1
+#define LO_ACK 2
+#define LO_NAK 3
+#define EXIT 4
+#define JOIN 5
+#define JN_ACK 6
+#define JN_NAK 7
+#define LEAVE_SESS 8
+#define NEW_SESS 9
+#define NS_ACK 10
+#define MESSAGE 11
+#define QUERY 12
+#define QU_ACK 13
+
+#define MAX_NAME 25
+#define MAX_DATA 1024
+
+struct message {
+    unsigned int type;
+    unsigned int size;
+    unsigned char source[MAX_NAME];
+    unsigned char data[MAX_DATA];
+};
+
 // main goal: to make use of UDP and send between client and server.
 /*
 Done by:
--- Lab4/server.c
+++ Lab4/server.c
@@ -9,6 +9,23 @@
 #include <netdb.h>
 #include <unistd.h>
 
+#define LOGIN 1
+#define LO_ACK 2
+#define LO_NAK 3
+#define EXIT 4
+#define JOIN 5
+#define JN_ACK 6
+#define JN_NAK 7
+#define LEAVE_SESS 8
+#define NEW_SESS 9
+#define NS_ACK 10
+#define MESSAGE 11
+#define QUERY 12
+#define QU_ACK 13
+
+#define MAX_NAME 25
+#define MAX_DATA 1024
+
 struct message {
     unsigned int type;
     unsigned int size;
